13a439f9ca82ef6b5fdfd841babecb7ed1e410d3
itganeshtech <it.ganesh.tech@gmail.com>
2022-02-19 14:02:19 +0530

    Add project files.

---
+++ Controllers/SiteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcAdoExample1.Models;
+using MvcAdoExample1.ViewModels;
+
+namespace MvcAdoExample1.Controllers
+{
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            StudentViewModel svm = new StudentViewModel();
+            List<Student> students = svm.GetAllStudents();
+            return View(students);
+        }
+    }
+}
---
+++ Models/Shared/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcAdoExample1.Models.Shared
+{
+    public class AppSettings
+    {
+        public static string ConnectionString()
+        {
+            return @"Data Source=DESKTOP-Q684RHH;Initial Catalog=TestDB2;Persist Security Info=True;User ID=sa;Password=sa123";
+        }
+    }
+}
